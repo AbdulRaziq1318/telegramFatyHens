ea32c22fe5d0a8f17c0f66553b4cdd984aada399
Abdul Raziq <abdulraziq1318@gmail.com>
2025-07-12 12:20:05 -0700

    Fix getTasks logic and Firestore issues

--- client/src/lib/getTasks.ts
+++ client/src/lib/getTasks.ts
@@ -1,11 +1,12 @@
-import { getDocs, collection, getFirestore } from 'firebase/firestore';
-import { app } from './firebaseConfig';
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { app } from "./firebaseConfig";
 
 export const getTasks = async () => {
   const db = getFirestore(app);
-  const snapshot = await getDocs(collection(db, 'tasks'));
-  return snapshot.docs.map(doc => ({
+  const querySnapshot = await getDocs(collection(db, "tasks"));
+  const tasks = querySnapshot.docs.map((doc) => ({
     id: doc.id,
     ...doc.data(),
   }));
+  return tasks;
 };
